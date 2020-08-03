0c468a716c5e2df9e9ea0707e509093a51cfd2dd
LH <lh@lhdv.net>
2020-08-03 19:51:53 -0300

    Change Home component to get data from backend and reder it

--- src/components/PageTemplate/index.js
+++ src/components/PageTemplate/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import Menu from '../Menu';
 import Footer from '../Footer';
@@ -11,18 +11,21 @@ const Main = styled.main`
     padding-top: 50px;
     padding-left: 5%;
     padding-right: 5%;
+    ${({ paddingAll }) => css`
+        padding: ${paddingAll};
+    `};
 `;
 
-function PageTemplate(props) {
-    return(
-        <>
-            <Menu />
-            <Main>
-                {props.children}
-            </Main>
-            <Footer />
-        </>
-    );
+function PageTemplate({ children, paddingAll }) {
+  return (
+    <>
+      <Menu />
+      <Main paddingAll={paddingAll}>
+        {children}
+      </Main>
+      <Footer />
+    </>
+  );
 }
 
 export default PageTemplate;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,69 +1,62 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import Menu from '../../components/Menu';
+
+import PageTempalte from '../../components/PageTemplate';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
 import categoriesRepo from '../../repositories/categories';
 
-import dadosIniciais from '../../data/dados_iniciais.json';
-
-const AppWrapper = styled.div`
-  background: var(--grayDark);
-`;
+// import dadosIniciais from '../../data/dados_iniciais.json';
 
 function Home() {
-
-  // const [dadosIniciais, setDadosIniciais] = useState({
-  //   categorias: [],
-  // });
+  const [dadosIniciais, setDadosIniciais] = useState([]);
 
   useEffect(() => {
     categoriesRepo.getAllCategoriesWithVideos()
       .then((categWithVideos) => {
-        console.log(categWithVideos);
+        // console.log(categWithVideos);
+        setDadosIniciais(categWithVideos);
       })
       .catch((err) => {
         console.log(err.message);
       });
   }, []);
+
   return (
-    <AppWrapper>
-      <Menu />
+    <PageTempalte paddingAll={0}>
+      { dadosIniciais.length === 0 && <div>Loading...</div>}
 
-      <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
-      />
+      { dadosIniciais.map((categ, idx) => {
+        if (idx === 0) {
+          return (
+            <div key={categ.id}>
+              <BannerMain
+                videoTitle={categ.videos[0].titulo}
+                url={categ.videos[0].url}
+                videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={categ}
+              />
+            </div>
+          );
+        }
 
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+        return (
+          <Carousel
+            key={categ.id}
+            category={categ}
+          />
+        );
+      }) }
 
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
+      { dadosIniciais.length >= 1 && (
+        <>
 
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />
+        </>
+      ) }
 
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[5]}
-      />
-
-      <Footer />
-    </AppWrapper>
+    </PageTempalte>
   );
 }
 
